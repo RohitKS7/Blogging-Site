2a4aae3a2713bc0b605c72c7a6882954c639ae47
Rohit Kumar Suman <99113868+RohitKS7@users.noreply.github.com>
2022-04-17 23:59:47 +0530

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,21 +1,3 @@
-
-
-tinymce.init({
-    selector: '#mytextarea',
-    height: 500,
-    plugins: [
-        'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
-        'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen', 'insertdatetime',
-        'media', 'table', 'emoticons', 'template', 'help', 'quickbars',
-    ],
-    toolbar: 'insertfile undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons | quickimage ',
-    menu: {
-        favs: { title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons' }
-    },
-    menubar: 'favs file edit view insert format tools table help',
-    content_css: 'css/content.css',
-});
-
 document.addEventListener('DOMContentLoaded', function () {
     let button = document.querySelector('.btn');
     let blogCon = document.querySelector('.blog');
@@ -23,8 +5,5 @@ document.addEventListener('DOMContentLoaded', function () {
     button.addEventListener( 'click', (e)=>{
         console.log(e);
         blogCon.innerHTML = tinyMCE.get('mytextarea').getContent();
-    })
-})
-
-
-
+    });
+});
