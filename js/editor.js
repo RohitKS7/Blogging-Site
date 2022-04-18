20a65b87ac80b1281ac709f2ff858556164ccecd
Rohit kumar suman <rohitkumarsuman2011india@gmail.com>
2022-04-18 13:12:53 +0530

    added some responsiveness

--- js/editor.js
+++ js/editor.js
@@ -1,9 +1,15 @@
+// trying what happens if i submit the content -
+// this function is only a test function to see that content of editor can be easily send by publish button.
+// the main function shall publish this content in deso blockchain.
 document.addEventListener('DOMContentLoaded', function () {
     let button = document.querySelector('.btn');
     let blogCon = document.querySelector('.blog');
 
-    button.addEventListener( 'click', (e)=>{
-        console.log(e);
-        blogCon.innerHTML = tinyMCE.get('mytextarea').getContent();
-    });
+    newFunction(button, blogCon);
 });
+function newFunction(button, blogCon) {
+    button.addEventListener('click', (e) => {
+        blogCon.innerHTML = tinyMCE.get('mytextarea').getContent();
+        e.preventDefault();
+    });
+}
